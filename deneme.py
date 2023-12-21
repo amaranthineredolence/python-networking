35f513ede78ea546748b07bfa72e1e7f100e9753
Baha Erten <66516780+amaranthineredolence@users.noreply.github.com>
2023-12-21 09:47:12 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,95 +1,97 @@
 import tkinter as tk
 from tkinter import filedialog
 import paramiko
+import threading
 import getpass
 import logging
+
+logging.basicConfig(filename='config_log.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class NetworkConfigurator:
     def __init__(self, master):
         self.master = master
-        master.title("Network Device Configurator")
+        self.master.title("Network Device Configurator")
 
-        self.username_label = tk.Label(master, text="Username:")
-        self.username_label.grid(row=0, column=0)
-        self.username_entry = tk.Entry(master)
-        self.username_entry.grid(row=0, column=1)
+        # Variables to store user inputs
+        self.username = tk.StringVar()
+        self.password = tk.StringVar()
+        self.key_path = tk.StringVar()
+        self.device_file_path = tk.StringVar()
+        self.config_commands_file_path = tk.StringVar()
 
-        self.password_label = tk.Label(master, text="Password:")
-        self.password_label.grid(row=1, column=0)
-        self.password_entry = tk.Entry(master, show="*")
-        self.password_entry.grid(row=1, column=1)
+        # GUI elements
+        tk.Label(master, text="Username:").grid(row=0, column=0, sticky=tk.W)
+        tk.Entry(master, textvariable=self.username).grid(row=0, column=1, sticky=tk.W)
 
-        self.key_label = tk.Label(master, text="SSH Key (optional):")
-        self.key_label.grid(row=2, column=0)
-        self.key_entry = tk.Entry(master)
-        self.key_entry.grid(row=2, column=1)
+        tk.Label(master, text="Password:").grid(row=1, column=0, sticky=tk.W)
+        tk.Entry(master, textvariable=self.password, show="*").grid(row=1, column=1, sticky=tk.W)
 
-        self.key_button = tk.Button(master, text="Browse", command=self.browse_ssh_key)
-        self.key_button.grid(row=2, column=2)
+        tk.Label(master, text="SSH Key Path:").grid(row=2, column=0, sticky=tk.W)
+        tk.Entry(master, textvariable=self.key_path).grid(row=2, column=1, sticky=tk.W)
+        tk.Button(master, text="Browse", command=self.browse_key).grid(row=2, column=2, sticky=tk.W)
 
-        self.file_label = tk.Label(master, text="Devices and Commands File:")
-        self.file_label.grid(row=3, column=0)
-        self.file_entry = tk.Entry(master)
-        self.file_entry.grid(row=3, column=1)
+        tk.Label(master, text="Device File Path:").grid(row=3, column=0, sticky=tk.W)
+        tk.Entry(master, textvariable=self.device_file_path).grid(row=3, column=1, sticky=tk.W)
+        tk.Button(master, text="Browse", command=self.browse_device_file).grid(row=3, column=2, sticky=tk.W)
 
-        self.file_button = tk.Button(master, text="Browse", command=self.browse_file)
-        self.file_button.grid(row=3, column=2)
+        tk.Label(master, text="Config Commands File Path:").grid(row=4, column=0, sticky=tk.W)
+        tk.Entry(master, textvariable=self.config_commands_file_path).grid(row=4, column=1, sticky=tk.W)
+        tk.Button(master, text="Browse", command=self.browse_config_commands_file).grid(row=4, column=2, sticky=tk.W)
 
-        self.run_button = tk.Button(master, text="Run Configuration", command=self.run_configuration)
-        self.run_button.grid(row=4, column=1)
+        tk.Button(master, text="Configure Devices", command=self.configure_devices).grid(row=5, column=0, columnspan=3)
 
-        # Set up logging
-        logging.basicConfig(filename="configurator_log.txt", level=logging.INFO,
-                            format="%(asctime)s - %(levelname)s - %(message)s")
+    def browse_key(self):
+        key_path = filedialog.askopenfilename(filetypes=[("SSH Key Files", "*.pem;*.key")])
+        if key_path:
+            self.key_path.set(key_path)
 
-    def browse_ssh_key(self):
-        ssh_key_path = filedialog.askopenfilename(title="Select SSH Key File")
-        self.key_entry.delete(0, tk.END)
-        self.key_entry.insert(0, ssh_key_path)
+    def browse_device_file(self):
+        device_file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
+        if device_file_path:
+            self.device_file_path.set(device_file_path)
 
-    def browse_file(self):
-        file_path = filedialog.askopenfilename(title="Select Devices and Commands File")
-        self.file_entry.delete(0, tk.END)
-        self.file_entry.insert(0, file_path)
+    def browse_config_commands_file(self):
+        config_commands_file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
+        if config_commands_file_path:
+            self.config_commands_file_path.set(config_commands_file_path)
 
-    def run_configuration(self):
-        username = self.username_entry.get()
-        password = self.password_entry.get()
-        ssh_key_path = self.key_entry.get()
-        devices_file_path = self.file_entry.get()
+    def configure_devices(self):
+        username = self.username.get()
+        password = self.password.get()
+        key_path = self.key_path.get()
+        device_file_path = self.device_file_path.get()
+        config_commands_file_path = self.config_commands_file_path.get()
 
         try:
-            with open(devices_file_path, 'r') as file:
-                devices_and_commands = [line.strip().split() for line in file.readlines()]
+            with open(device_file_path, 'r') as device_file:
+                devices = device_file.read().splitlines()
 
-            for device_info in devices_and_commands:
-                ip_address = device_info[0]
-                commands = device_info[1:]
+            with open(config_commands_file_path, 'r') as commands_file:
+                commands = commands_file.read().splitlines()
 
+            for device in devices:
                 try:
                     ssh = paramiko.SSHClient()
                     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-                    if ssh_key_path:
-                        private_key = paramiko.RSAKey(filename=ssh_key_path)
-                        ssh.connect(ip_address, username=username, pkey=private_key)
+                    if key_path:
+                        private_key = paramiko.RSAKey(filename=key_path)
+                        ssh.connect(device, username=username, pkey=private_key)
                     else:
-                        ssh.connect(ip_address, username=username, password=password)
+                        ssh.connect(device, username=username, password=password)
 
                     for command in commands:
                         stdin, stdout, stderr = ssh.exec_command(command)
-                        logging.info(f"Device {ip_address} - Command: {command} - Status: {stdout.channel.recv_exit_status()}")
+                        logging.info(f"Device: {device}, Command: {command}, Status: Success")
 
-                    logging.info(f"Configuration completed for device {ip_address}")
+                    ssh.close()
+
                 except Exception as e:
-                    logging.error(f"Failed to configure device {ip_address}. Error: {str(e)}")
-                finally:
-                    if ssh:
-                        ssh.close()
+                    logging.error(f"Device: {device}, Error: {str(e)}")
+                    continue
 
-            logging.info("Configuration process completed.")
         except Exception as e:
-            logging.error(f"Failed to read devices and commands file. Error: {str(e)}")
+            logging.error(f"Error: {str(e)}")
 
 
 if __name__ == "__main__":
